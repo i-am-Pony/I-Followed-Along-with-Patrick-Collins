b984fe11f50a2713061a5b9a201db72df3bbe517
i-am-Pony <109096522+i-am-Pony@users.noreply.github.com>
2022-11-28 17:07:49 -0500

    This was Lesson 3
    Lesson 3 and second contract built using this course. This contract works in conjunction with the previous contract SimpleStorage. It's main functions are to call a contract to deploy within a contract create, then create an array of contracts deployed, and store their individual address and number.

---
+++ StorageFactory.sol
@@ -0,0 +1,40 @@
+// choose your license type
+// SPDX-License-Identifier: MIT
+
+// declare your compiler, amke sure it is compatible
+// with any other contracts you plan to interact with
+pragma solidity ^0.8.0;
+
+// import the SimpleStorage contract to interact with
+// and ensure proper functionality
+import "./SimpleStorage.sol";
+
+// name your contarct
+contract StorageFactory {
+
+// you can create an array like the one below for the
+// function that follows it
+    SimpleStorage[] public simpleStorageArray;
+
+// this (in conjunction with the above array deploy a
+// new contract within a contract and push (create) a
+// and store a new simple storage each time you call it  
+    function createSimplyStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    // whenever you interact with a contraxt you need 1) the address 2) the ABI
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address 
+        // ABI 
+        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+   }
+
+   function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+
+}
